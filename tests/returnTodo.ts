f757036cb3a8037ec40c82cc27f4ad5e9f8c3428
KobayashiAnzu <anzu020618@gmail.com>
2024-05-16 15:12:57 +0900

    スクリーンショットを撮りワークフローに取り入れる

--- tests/returnTodo.ts
+++ tests/returnTodo.ts
@@ -5,4 +5,7 @@ export { clickReturnButton };
 /**戻すボタンをクリック*/
 async function clickReturnButton(page: Page) {
   await page.getByRole("button", { name: "戻す" }).click();
+  //スクリーンショットを撮る
+  await page.waitForTimeout(2000);
+  await page.screenshot({ path: "return.png" });
 }
